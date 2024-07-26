476bae0a5c93c7380df702df31d51cb1443089c4
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2024-07-27 01:05:29 +0300

    Update admin

--- app/tree_menu/admin.py
+++ app/tree_menu/admin.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 from tree_menu.models import Menu, MenuItem
 
 
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    extra = 1
+
+
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('title',)
+    inlines = (MenuItemInline,)
 
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'menu')
+    list_display = ('title', 'parent_item', 'menu')
+    list_filter = ('menu',)
